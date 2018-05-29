0368427644d1a27c387772e9ea878470cd0b821a
Bill Tihen <btihen@gmail.com>
2018-05-29 19:44:20 +0200

    updated tests: - moved auth tests from connection to integrated - added a few more tests to integrated

---
+++ spec/integrated_spec.rb
@@ -0,0 +1,59 @@
+require "spec_helper"
+
+# STATE AFTER AUTHENTICATION - notice: token_expires (field)
+# @credentials=
+#  {:base_uri=>"https://las-test.powerschool.com",
+#   :auth_endpoint=>"/oauth/access_token",
+#   :client_id=>"xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+#   :client_secret=>"xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+#   :token_expires=>2018-02-18 16:47:28 +0200,
+#   :access_token=>"xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"},
+# @options=
+#  {:headers=>
+#    {"User-Agent"=>"Ruby Powerschool",
+#     "Accept"=>"application/json",
+#     "Content-Type"=>"application/json",
+#     "Authorization"=>"Bearer xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"}}>
+
+# GET YOUR CONNECTION INFO FROM:
+# System>System Settings>Plugin Management Configuration>your plugin>Data_Provider_Configuration
+
+RSpec.describe "Actual Communication Tests" do
+
+  context "PowerSchool connection success" do
+    it "authenicates by default" do
+      ps_new = PsUtilities::Connection.new
+      expect { ps_new.run }.not_to raise_error()
+    end
+    it "authenicates with command" do
+      ps_new = PsUtilities::Connection.new
+      expect { ps_new.run(command: :authenticate) }.not_to raise_error()
+    end
+    it "authenticates gets oauth token" do
+      ps_new = PsUtilities::Connection.new
+      ps_new.run
+      answer = ps_new.credentials[:access_token]
+      expect( answer ).not_to be nil
+    end
+    it "authenticates has expiration time" do
+      ps_new = PsUtilities::Connection.new
+      ps_new.run
+      answer = ps_new.credentials[:token_expires]
+      expect( answer ).not_to be nil
+    end
+    xit "run(command: :authenticate) - returns expected data type" do
+
+    end
+  end
+
+  context "PowerSchool connection success" do
+
+    let!(:ps_auth) { PsUtilities::Connection.new.run }
+
+    xit "run(command: :xxxx) return expected data type" do
+
+    end
+  end
+
+
+end
--- spec/ps_utilities/connection_spec.rb
+++ spec/ps_utilities/connection_spec.rb
@@ -1,57 +1,77 @@
 require "spec_helper"
 
+# BEFORE AUTHENTATION
+# @credentials=
+#  {:base_uri=>"https://las-test.powerschool.com",
+#   :auth_endpoint=>"/oauth/access_token",
+#   :client_id=>"xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+#   :client_secret=>"xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"},
+# @options=
+#  {:headers=>{"User-Agent"=>"Ruby Powerschool", "Accept"=>"application/json", "Content-Type"=>"application/json"}}>
+
 RSpec.describe PsUtilities::Connection do
 
-  context "server parameters are correct" do
-    it "shows correct base_uri from parameters" do
+  context "server configures with ENV-VARS" do
+    it "Instantiates with minimal ENV-VARS" do
       stub_const('ENV', ENV.to_hash.merge('PS_URL' => 'ps_url'))
-      ps = PsUtilities::Connection.new({attributes: {base_uri: 'params_host'}})
-      expect(ps.credentials[:base_uri]).to eq('params_host')
+      stub_const('ENV', ENV.to_hash.merge('PS_CLIENT_ID' => 'client_id'))
+      stub_const('ENV', ENV.to_hash.merge('PS_CLIENT_SECRET' => 'client_secret'))
+      expect { PsUtilities::Connection.new }.not_to raise_error()
     end
-    it "shows correct base_uri from environment" do
+    it "base_uri" do
       stub_const('ENV', ENV.to_hash.merge('PS_URL' => 'ps_url'))
       ps = PsUtilities::Connection.new
       expect(ps.credentials[:base_uri]).to eq(ENV['PS_URL'])
     end
-    it "shows correct auth_endpoint from parameters" do
-      stub_const('ENV', ENV.to_hash.merge('PS_ATUH_ENDPOINT' => 'auth_endpoint'))
-      ps = PsUtilities::Connection.new({attributes: {auth_endpoint: 'endpoint'}})
-      expect(ps.credentials[:auth_endpoint]).to eq('endpoint')
-    end
-    it "shows correct auth_endpoint from environment" do
+    it "auth_endpoint" do
       stub_const('ENV', ENV.to_hash.merge('PS_AUTH_ENDPOINT' => 'auth_endpoint'))
       ps = PsUtilities::Connection.new
       expect(ps.credentials[:auth_endpoint]).to eq(ENV['PS_AUTH_ENDPOINT'])
     end
-    it "shows correct client_id from parameters" do
-      stub_const('ENV', ENV.to_hash.merge('PS_CLIENT_ID' => 'client_id'))
-      ps = PsUtilities::Connection.new({attributes: {client_id: 'id'}})
-      expect(ps.credentials[:client_id]).to eq('id')
-    end
-    it "shows correct client_id from environment" do
+    it "client_id" do
       stub_const('ENV', ENV.to_hash.merge('PS_CLIENT_ID' => 'client_id'))
       ps = PsUtilities::Connection.new
       expect(ps.credentials[:client_id]).to eq(ENV['PS_CLIENT_ID'])
     end
-    it "shows correct client_secret from parameters" do
-      stub_const('ENV', ENV.to_hash.merge('PS_CLIENT_SECRET' => 'client_secret'))
-      ps = PsUtilities::Connection.new({attributes: {client_secret: 'secret'}})
-      expect(ps.credentials[:client_secret]).to eq('secret')
-    end
-    it "shows correct client_secret from environment" do
+    it "client_secret" do
       stub_const('ENV', ENV.to_hash.merge('PS_CLIENT_SECRET' => 'client_secret'))
       ps = PsUtilities::Connection.new
       expect(ps.credentials[:client_secret]).to eq(ENV['PS_CLIENT_SECRET'])
     end
-    it "shows correct access_token from parameters" do
-      stub_const('ENV', ENV.to_hash.merge('PS_ACCESS_TOKEN' => 'access_token'))
-      ps = PsUtilities::Connection.new({attributes: {access_token: 'token'}})
-      expect(ps.credentials[:access_token]).to eq('token')
-    end
-    it "shows correct access_token from environment" do
+    # not recommended - changes - shouldn't be part of ENV-VARS
+    xit "access_token" do
       stub_const('ENV', ENV.to_hash.merge('PS_ACCESS_TOKEN' => 'access_token'))
       ps = PsUtilities::Connection.new
       expect(ps.credentials[:access_token]).to eq(ENV['PS_ACCESS_TOKEN'])
+    end
+  end
+
+  context "server parameters override ENV-VARS" do
+    it "base_uri" do
+      stub_const('ENV', ENV.to_hash.merge('PS_URL' => 'ps_url'))
+      ps = PsUtilities::Connection.new({attributes: {base_uri: 'params_host'}})
+      expect(ps.credentials[:base_uri]).to eq('params_host')
+    end
+    it "auth_endpoint" do
+      stub_const('ENV', ENV.to_hash.merge('PS_ATUH_ENDPOINT' => 'auth_endpoint'))
+      ps = PsUtilities::Connection.new({attributes: {auth_endpoint: 'endpoint'}})
+      expect(ps.credentials[:auth_endpoint]).to eq('endpoint')
+    end
+    it "client_id" do
+      stub_const('ENV', ENV.to_hash.merge('PS_CLIENT_ID' => 'client_id'))
+      ps = PsUtilities::Connection.new({attributes: {client_id: 'id'}})
+      expect(ps.credentials[:client_id]).to eq('id')
+    end
+    it "client_secret" do
+      stub_const('ENV', ENV.to_hash.merge('PS_CLIENT_SECRET' => 'client_secret'))
+      ps = PsUtilities::Connection.new({attributes: {client_secret: 'secret'}})
+      expect(ps.credentials[:client_secret]).to eq('secret')
+    end
+    # OK AS A pass-in param
+    it "access_token" do
+      # stub_const('ENV', ENV.to_hash.merge('PS_ACCESS_TOKEN' => 'access_token'))
+      ps = PsUtilities::Connection.new({attributes: {access_token: 'token'}})
+      expect(ps.credentials[:access_token]).to eq('token')
     end
   end
 
@@ -68,21 +88,6 @@ RSpec.describe PsUtilities::Connection do
       stub_const('ENV', ENV.to_hash.merge('PS_CLIENT_SECRET' => nil))
       expect { PsUtilities::Connection.new }.to raise_error(ArgumentError, /missing client_secret/)
     end
-  end
-
-  context "PowerSchool connection" do
-    it "builds a connection to PowerSchool" do
-      expect { PsUtilities::Connection.new }.not_to raise_error()
-    end
-    it "authenticates to PowerSchool" do
-      ps = PsUtilities::Connection.new
-      expect { ps.run }.not_to raise_error()
-      # ps = PsUtilities::Connection.new
-      # response = ps.run
-      # not to raise error - eventually you will want data
-      # expect(response).to be_truthy
-    end
-    it ""
   end
 end
 
