8c494c86c19f6f74cb61ed2d434a797ebdd15cc0
Bill Tihen <btihen@gmail.com>
2018-06-22 23:46:07 +0200

    version bump

--- lib/ps_utilities/version.rb
+++ lib/ps_utilities/version.rb
@@ -1,5 +1,5 @@
 module PsUtilities
   module Version
-    VERSION = "0.3.0"
+    VERSION = "0.3.1"
   end
 end
--- spec/ps_utilities/version_spec.rb
+++ spec/ps_utilities/version_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe PsUtilities do
   end
 
   it "has the correct version number" do
-    expect(PsUtilities::Version::VERSION).to eq "0.3.0"
+    expect(PsUtilities::Version::VERSION).to eq "0.3.1"
   end
 
 end
